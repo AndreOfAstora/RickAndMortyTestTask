4194e2ac791f37c2e8b8d013af6d87bf6d40e9c2
Andrew <goldsta442@gmail.com>
2022-01-22 17:22:24 +0200

    CharacterList transferd to Redux hooks

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,28 +1,29 @@
 import { useEffect } from "react";
-import { connect } from "react-redux";
+import { connect, shallowEqual } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getAllCharacters, getAllCharactersByKeyword } from "../../actions/characterListActions";
 import Character from "./Character/Character";
 
 
 const CharacterList = (props) => {
-  const {
-    characters,
-    getAllCharacters,
-    getAllCharactersByKeyword
-  } = props;
+  
+  const dispatch = useDispatch();
+
+  const characters = useSelector(state => state.characterList.characters);
 
   let params = useParams();  
 
   useEffect(() => {    
     if (params.keyword === undefined) {      
-      getAllCharacters();
+      dispatch(getAllCharacters());
     } else {      
-      getAllCharactersByKeyword(params.keyword);
+      dispatch(getAllCharactersByKeyword(params.keyword));
     }    
   }, [params]);
 
-
+  
   return (
     <>
       <p>charlist</p>
@@ -33,17 +34,6 @@ const CharacterList = (props) => {
   )
 }
 
-const mapState = (state) => {
-  return {
-    characters: state.characterList.characters
-  }
-}
 
-const mapDispatch = (dispatch) => {
-  return {
-    getAllCharacters: () => dispatch(getAllCharacters()),
-    getAllCharactersByKeyword: keyword => dispatch(getAllCharactersByKeyword(keyword))
-  }
-}
 
-export default connect(mapState, mapDispatch)(CharacterList);
+export default CharacterList;
