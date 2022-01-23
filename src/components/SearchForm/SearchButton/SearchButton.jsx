b3215be25aef0b31ce4194056a220091d4d105d6
Andrew <goldsta442@gmail.com>
2022-01-23 22:02:05 +0200

    Button in SearchButton.jsx replaced with MUI button.

--- src/components/SearchForm/SearchButton/SearchButton.jsx
+++ src/components/SearchForm/SearchButton/SearchButton.jsx
@@ -1,25 +1,30 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import Button from '@mui/material/Button';
 
 // TODO:
 // 1) Navigate using inputTexn in store, make sure, it's set before autocomplete is fetched
 
-const SearchButton = () => { 
+const SearchButton = () => {
 
   let navigate = useNavigate();
 
-  const keyword = useSelector(state => 
-    state.search.autocomplete.keyword    
+  const keyword = useSelector(state =>
+    state.search.autocomplete.keyword
   );
-  
 
-  const handleBtnClick = () => {    
+
+  const handleBtnClick = () => {
     navigate(`search/${keyword}`);
   }
 
-  return(
-    <button onClick={handleBtnClick}>search</button>
-  )
+  return (<>
+    <Button
+      variant='outlined'
+      onClick={handleBtnClick}>
+        search
+    </Button>
+  </>)
 }
 
 
